ebf09df0fd048e50f1930924be5cae4b5857f3fb
Do Hoerin <hellodhlyn@gmail.com>
2018-01-17 01:43:57 +0900

    Added nvm setup instructions

--- osx-setup-devenv.sh
+++ osx-setup-devenv.sh
@@ -10,6 +10,7 @@ git clone git://github.com/scmbreeze/scm_breeze.git ~/.scm_breeze
 ~/.scm_breeze/install.sh
 
 # Install pyenv
+# See: https://github.com/pyenv/pyenv
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
 echo '# Pyenv configurations' >> ~/.zshrc
@@ -18,9 +19,21 @@ echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
 echo '' >> ~/.zshrc
 
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
+source ~/.zshrc
 
 pyenv init
 pyenv install 3.6.4  # TODO: get latest version of python 3
 pyenv global 3.6.4
+
+# Install nvm
+# See: https://github.com/creationix/nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+
+source ~/.zshrc
+
+nvm install --lts
+nvm use --lts
+nvm alias default node
+
+
+echo 'Setup completed. Reload shell using command "zsh".'
--- osx-setup-terminal.sh
+++ osx-setup-terminal.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Install Oh-My-Zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 # Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
@@ -10,5 +7,10 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
+# Install Oh-My-Zsh
+curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+
 # Set environment vars & aliases
-cat ./dotfiles/zshrc >> ~/.zshrc
+cat ./dotfiles/.zshrc >> ~/.zshrc
+
+echo 'Setup completed. Reload shell using command "zsh".'
