c2446c98dd359da846eadf1352b87d6478a36fe6
Hoerin Doh <hellodhlyn@gmail.com>
2022-04-06 21:11:03 +0900

    Add GPG configs for git

--- setups/apps/git.sh
+++ setups/apps/git.sh
@@ -2,5 +2,8 @@
 
 git config --global user.email 'hellodhlyn@gmail.com'
 git config --global user.name 'Hoerin Doh'
+git config --global user.signingkey '8A82A50A2DF52EE4'
 git config --global core.quotepath off
 git config --global pull.rebase false
+git config --global commit.gpgsign true
+git config --global tag.gpgsign true
