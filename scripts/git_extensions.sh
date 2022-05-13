1494083a0a6444ea27059bc32fef462fdd6f53b8
Hoerin Doh <hellodhlyn@gmail.com>
2022-05-13 15:57:08 +0900

    Improve gitmg command

--- scripts/git_extensions.sh
+++ scripts/git_extensions.sh
@@ -13,13 +13,17 @@ function gitrb() {
 }
 
 function gitmg() {
-  current_branch=$(git branch --show-current)
-  if [[ $current_branch == "main" ]]; then
+  current_branch="$(git branch --show-current)"
+  if [[ "$current_branch" == "main" ]]; then
     return
   fi
 
+  pull_request="$(gh pr view --json number,title)"
+  pr_number="$(echo $pull_request | jq -r .number)"
+  pr_title="$(echo $pull_request | jq -r .title)"
+
   git switch main && \
-    git merge --no-ff $current_branch && \
+    git merge --no-ff $current_branch -m "Merge #$pr_number: $pr_title" && \
     git push origin main && \
     git branch -d $current_branch
 }
@@ -27,4 +31,3 @@ function gitmg() {
 function gitlog() {
   git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset' --abbrev-commit --all
 }
-
